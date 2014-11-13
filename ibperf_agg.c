466da181a5ef398448c466168856d5ed9546646a
Susan Coulter <markus@lanl.gov>
2014-11-13 09:53:58 -0700

    Update ibperf_agg.c

--- ibperf_agg.c
+++ ibperf_agg.c
@@ -1,11 +1,12 @@
 /* ---------------------------------------------------------------
  ** IB Performance testing
- ** FILE: ib_bw.c
+ ** FILE: ibperf_agg.c
  ** DESCRIPTION: This program runs client/server IB testing on  
- ** all the nodes in the job allocation.  Aggregate
+ ** all the nodes in the job allocation in an aggregate manner.
+ ** If N nodes in the allocation, N-1 nodes communicate with the 
+ ** same single node; for each node in the allocation.
  **
  **  AUTHOR: Susan Coulter
- **  DATE: 2013-03-07
  ** --------------------------------------------------------------- */
 
 #include "mpi.h"
@@ -14,8 +15,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <float.h> 
-#define NODEFILE      "/usr/projects/systems/skc/cj_gazebo/test_exec/ibperf_agg/src/nodefile"
-#define MAX_NODES     10000
+#include <sys/time.h> 
+#define BW_COLUMN	"4"
+#define BW_MSG_SIZE	"65536"
+#define DEFAULT_TEST	"ib_read_bw"
+#define LAT_COLUMN	"5"
+#define LAT_MSG_SIZE	"8"
+#define MAX_NODES	10000
+#define MAX_ARGS	3
+#define NODEFILE	"/yellow/users/markus/mu_gazebo/test_exec/ibperf_ring/src/nodefile"
+#define READ_NUM_RUNS	"1000"
+#define WRITE_NUM_RUNS	"5000"
 
 #define MPICHK(_ret_)                                                        \
 {if (MPI_SUCCESS != (_ret_))                                                 \
@@ -52,34 +62,89 @@ char *argv[];
    FILE *fpnode;
    FILE *fptest;
 
-   char cmd[15], host[10], input[10], result[80], test[50];
-   int dfltport=18515;
-   int client, clnport, gbuf_size, gnum, ibx, idx, myrank, numranks, rc, server, svrport;
+   char cmd[40], column[2], host[10], input[10], msg_size[8], num_runs[8], nodefile[80], result[80], rm[100], test[50];
    double *gbuf, *gp;
-   double aggtot, avg, num, tot, EndTime, RunTime, StartTime;
+   double avgtot, avg, num, rfrom, EndTime, RunTime, StartTime;
+   int dfltport=18515;
+   int client, clnport, eidx, i, ibx, idx, ridx;
+   int myrank, numranks, rc, server, svrport;
+
+   struct timeval tv;
 
    MPI_Status status;
    MPI_Request sndrq;
 
-/* can override default ib test */
+/* setup test - assume default   */
+/*   if args, set as appropriate */
 
-   if ( argc == 2 ) {
-     strncpy(cmd, argv[1], sizeof cmd);
-   } else {
-     strncpy(cmd, "ib_read_bw", sizeof cmd);
-   }
- 
+   strncpy(cmd, DEFAULT_TEST, sizeof cmd);
+   strcpy(column, "$");
+   if (argc > 1) {
+      strncpy(cmd, argv[1], sizeof cmd);
+      if (strstr(cmd,"ib_read"))
+        strncpy(num_runs, READ_NUM_RUNS, sizeof num_runs);
+      if (strstr(cmd,"ib_write"))
+        strncpy(num_runs, WRITE_NUM_RUNS, sizeof num_runs);
+
+      if (strstr(cmd,"bw")) {
+        strcat(column, BW_COLUMN);
+        strncpy(msg_size, BW_MSG_SIZE, sizeof msg_size);
+      }
+      if (strstr(cmd,"lat")) {
+        strcat(column, LAT_COLUMN);
+        strncpy(msg_size, LAT_MSG_SIZE, sizeof msg_size);
+      }
+
+      if (!(!strcmp(cmd,"ib_read_bw") || !strcmp(cmd,"ib_write_bw") || !strcmp(cmd,"ib_read_lat") || !strcmp(cmd,"ib_write_lat"))) {
+          strncpy(exitmsg, "Unknown test requested, stopping...", sizeof exitmsg);
+          bad_exit(exitmsg);
+      }
+
+      for (idx=2; idx < argc; idx++) {
+        if (strlen(argv[idx]) > 7) {
+          strncpy(exitmsg, "Message size or number of runs > 7 digits, stopping...", sizeof exitmsg);
+          bad_exit(exitmsg);
+        }
+        if (idx==2) strncpy(msg_size, argv[idx], sizeof msg_size);
+        if (idx==3) strncpy(num_runs, argv[idx], sizeof num_runs);
+        if (idx>MAX_ARGS) {
+          strncpy(exitmsg, "Too many parameters, stopping...", sizeof exitmsg);
+          bad_exit(exitmsg);
+        }
+      }
+    } else {
+      strncpy(msg_size, BW_MSG_SIZE, sizeof msg_size);
+      strncpy(num_runs, READ_NUM_RUNS, sizeof num_runs);
+      strcat(column, BW_COLUMN);
+    }
+
+/* build full test command */
+
+   strcat(cmd, " -s ");
+   strncat(cmd, msg_size, sizeof msg_size);
+   strcat(cmd, " -n ");
+   strncat(cmd, num_runs, sizeof num_runs);
+
    MPICHK(MPI_Init(&argc,&argv));
    MPICHK(MPI_Comm_size(MPI_COMM_WORLD,&numranks));
    MPICHK(MPI_Comm_rank(MPI_COMM_WORLD,&myrank));
 
 /* run specific variables */
+   
+   sprintf(nodefile, "%s.%s", NODEFILE, getenv("SLURM_JOBID"));
 
-   gnum=0;
-   gnum = numranks - 1;
-   gbuf_size = numranks * gnum;
-   double rnums[gnum];
+   double rto[numranks],tot[numranks];
    string nodes[numranks];
+
+/* initialize arrays and counters to 0 */
+
+   avgtot = 0;
+   rfrom = 0;
+   idx=0;
+   while (idx < numranks) {
+      rto[idx] = tot[idx] = 0;
+      idx++;
+   }
 
 /* get and print host info */
 
@@ -95,7 +160,7 @@ char *argv[];
    while (idx <= numranks) {
          MPI_Barrier(MPI_COMM_WORLD);
       if (idx == 0 && myrank == 0) {
-         if ((fpnode=fopen(NODEFILE,"w"))==NULL) {
+         if ((fpnode=fopen(nodefile,"w"))==NULL) {
            strncpy(exitmsg, "Failure opening NODEFILE for WRITE", sizeof exitmsg);
            bad_exit(exitmsg);
          }
@@ -105,7 +170,7 @@ char *argv[];
          MPI_Barrier(MPI_COMM_WORLD);
       } else if (myrank == idx) {
          MPI_Barrier(MPI_COMM_WORLD);
-         if ((fpnode=fopen(NODEFILE,"a"))==NULL) {
+         if ((fpnode=fopen(nodefile,"a"))==NULL) {
            strncpy(exitmsg, "Failure opening NODEFILE for WRITE", sizeof exitmsg);
            bad_exit(exitmsg);
          }
@@ -123,8 +188,8 @@ char *argv[];
 
 /* everybody reads the node file */
 
-   if ((fpnode=fopen(NODEFILE,"r"))==NULL) {
-      strncpy(exitmsg, "Failure opening nodefile for READ", sizeof exitmsg);
+   if ((fpnode=fopen(nodefile,"r"))==NULL) {
+      strncpy(exitmsg, "Failure opening NODEFILE for READ", sizeof exitmsg);
       bad_exit(exitmsg);
    }
 
@@ -140,6 +205,8 @@ char *argv[];
         bad_exit(exitmsg);
       }
    }
+
+   fclose(fpnode);
 
    MPI_Barrier(MPI_COMM_WORLD);
    StartTime = MPI_Wtime();
@@ -170,7 +237,7 @@ char *argv[];
       while (client < numranks) {
         if (myrank == client && myrank != server) {
            clnport = dfltport + client;
-           sprintf(test,"%s -p %i %s | /bin/grep 65536 | /bin/sed 's/[ \t]*$//' | /bin/sed 's/65536.* //'", cmd, clnport, nodes[server]);
+           sprintf(test,"%s -p %i %s | /bin/grep %s | /bin/grep %s | /bin/grep -v QPN | /bin/awk '{print %s}'", cmd, clnport, nodes[server], msg_size, num_runs, column);
            if ((fptest=popen(test,"r")) == NULL) {
               strncpy(exitmsg, "failed to open pipe to run test", sizeof exitmsg);
               bad_exit(exitmsg);
@@ -178,11 +245,10 @@ char *argv[];
            
            fgets(result, 80, fptest);
            result[strlen(result)-1] = '\0';
-          
-           printf("<td> %s_From_%s_to_%s_%i_ranks %s\n", cmd, nodes[client], nodes[server], numranks, result);
            num=0;
            num = atof(result);
-           rnums[ibx] = num;  
+           rfrom = rfrom + num;  
+           rto[server] = rto[server] + num;  
            clnport++;
            ibx++;
         }
@@ -198,30 +264,77 @@ char *argv[];
 
    RunTime = EndTime - StartTime;
 
-/* allocate buffer on rank 0
- * get stats and calc average */
+   rfrom = rfrom / (numranks-1);
 
-   if (myrank ==0) 
-      gbuf = (double *)malloc(numranks*gnum*sizeof(double));
+/* DEBUG
 
-   MPICHK(MPI_Gather(rnums, gnum, MPI_DOUBLE, gbuf, gnum, MPI_DOUBLE, 0, MPI_COMM_WORLD));
+   printf("\nRank %i reporting: Avgerage of %#.2f\n\n", myrank, rfrom);
+   idx=0;
+   while (idx < numranks) {
+      printf("Rank %i reporting rto[%i] = %#.2f\n", myrank, idx, rto[idx]);
+      idx++;
+   }
+*/
 
+/* get and print client data */
+
+   if (myrank == 0) 
+      gbuf = (double *)malloc(numranks * sizeof(double));
+
+   MPICHK(MPI_Gather(&rfrom, 1, MPI_DOUBLE, gbuf, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD));
+ 
+   cmd[strcspn(cmd," ")] = '\0';
    if (myrank == 0) {
       idx=0;
       gp = gbuf;
-      while (idx < gbuf_size) {
-         tot = tot + *gp;
+      while (idx < numranks) {
+         printf("<td> %s_%s_%s_AggAvgFrom_%s_%iRanks %#.2f\n", cmd, num_runs, msg_size, nodes[idx], numranks, *gp); 
+         avgtot = avgtot + *gp;
          idx++;
          gp++;
       }
-      avg = tot / idx;
-      aggtot = avg * gnum;
-      printf("<td> %s_AggAvgInd_%iRanks %#.2f\n", cmd, numranks, avg);
-      printf("<td> %s_AggAvgTot_%iRanks %#.2f\n", cmd, numranks, aggtot);
+   }
+
+/* get and print server data */
+
+   if (myrank ==0) 
+      gbuf = (double *)malloc((numranks * numranks) * sizeof(double));
+
+   MPICHK(MPI_Gather(rto, numranks, MPI_DOUBLE, gbuf, numranks, MPI_DOUBLE, 0, MPI_COMM_WORLD));
+
+   if (myrank == 0) {
+      idx=eidx=ridx=0;
+      gp = gbuf;
+      while (idx < numranks*numranks) {
+         if (eidx == (numranks)) {
+            eidx=0;
+            ridx++;
+         }
+         if (eidx != ridx) {
+           tot[eidx] = tot[eidx] + *gp;
+         } 
+         avgtot = avgtot + *gp;
+         eidx++;
+         idx++;
+         gp++;
+      }
+
+      idx=0;
+      while (idx < numranks) {
+         printf("<td> %s_%s_%s_AggAvgTo_%s_%iRanks %#.2f\n", cmd, num_runs, msg_size, nodes[idx], numranks, tot[idx]/(numranks-1));
+         idx++;
+      }
+
+      avg = avgtot / ((numranks*(numranks-1)) + numranks);
+      printf("<td> %s_%s_%s_AggAvgInd_%iRanks %#.2f\n", cmd, num_runs, msg_size, numranks, avg);
+      printf("<td> %s_%s_%s_AggAvgTot_%iRanks %#.2f\n", cmd, num_runs, msg_size, numranks, avg * (numranks-1));
       printf("Aggregate run time %f\n", RunTime);
       printf("<results> PASS Job concluded successfully\n");
-   } 
 
+      sprintf(rm, "/bin/rm -f %s", nodefile);
+      system(rm);
+   } 
+ 
    MPI_Finalize();
-   exit(0);
+/*   exit(0); */
 }
